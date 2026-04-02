d7f59567298c0e4c4b35d7738a241246e0973d42
Farshy <farihafarshy2020@gmail.com>
2026-04-02 23:38:16 +0600

    change some line

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -98,7 +99,7 @@ async function run() {
 
 
 
-        
+
         // post wedding collection
 
         app.post("/wedding", async (req, res) => {
